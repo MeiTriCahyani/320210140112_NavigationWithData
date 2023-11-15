91d3661e44a12095beca76c251fd86447f75ec82
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-11-15 21:41:05 +0700

    Add spacer

--- app/src/main/java/com/example/pamlayout/HalamanForm.kt
+++ app/src/main/java/com/example/pamlayout/HalamanForm.kt
@@ -2,8 +2,10 @@ package com.example.pamlayout
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,6 +47,7 @@ fun HalamanFrom(
             Text(text = stringResource(id = R.string.almt))
 
         })
+        Spacer(modifier = Modifier.height(15.dp))
 
 
     }

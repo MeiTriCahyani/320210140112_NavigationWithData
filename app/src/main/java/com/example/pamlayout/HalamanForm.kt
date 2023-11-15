912e63cb51f4e5caddff3b30580af8e87a70ce6e
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-11-15 21:27:09 +0700

    Add outlined text field nama

--- app/src/main/java/com/example/pamlayout/HalamanForm.kt
+++ app/src/main/java/com/example/pamlayout/HalamanForm.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,6 +32,9 @@ fun HalamanFrom(
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
+        OutlinedTextField(value = namaTxt, onValueChange = { namaTxt = it }, label = {
+            Text(text = stringResource(id = R.string.name))
+        })
 
     }
 

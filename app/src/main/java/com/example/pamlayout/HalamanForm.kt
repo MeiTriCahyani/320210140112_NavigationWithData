ecf06796359e97bac304f7acae9a8216295f77d6
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2023-11-15 21:19:25 +0700

    Add var namaTxt, almtTxt, tlpnTxt

--- app/src/main/java/com/example/pamlayout/HalamanForm.kt
+++ app/src/main/java/com/example/pamlayout/HalamanForm.kt
@@ -2,11 +2,18 @@ package com.example.pamlayout
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanFrom(
     onSubmitButtonClicked: (MutableList<String>) -> Unit
 ){
+    var namaTxt by remember { mutableStateOf("") }
+    var almtTxt by remember { mutableStateOf("") }
+    var tlpnTxt by remember { mutableStateOf("") }
 
 }
